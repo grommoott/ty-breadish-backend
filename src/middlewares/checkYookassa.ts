08c7236914113523286460d87a16092331dc8add
grommoott <grommoott@gmail.com>
2024-08-17 12:11:03 +0700

    yookassa delta

--- src/middlewares/checkYookassa.ts
+++ src/middlewares/checkYookassa.ts
@@ -1,15 +1,11 @@
 import { Middleware } from "./middleware";
 
 const ips: Array<string> = new Array(
-    "77.75.154.206",
-    "10.219.43.124",
-    "10.220.132.239"
+    "77.75.154.206"
 )
 
 const checkYookassa: Middleware = (req, res, next) => {
-    const ip: string | undefined = req.header("x-forwarded-for")?.split(", ").pop()
-
-    console.log(req)
+    const ip: string | undefined = req.header("true-client-ip")?.split(", ").pop()
 
     if (ips.findIndex((val) => val == ip) == -1) {
         next(new Error("Forbidden!", { cause: 403 }))
