346d79b9d2f4c2fb0ec04bb9038604199f58748e
grommoott <grommoott@gmail.com>
2024-08-20 18:52:10 +0700

    test2

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 
 function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
-    res.sendStatus(error.cause as number || 400)
-
     if (error.cause === 500) {
         next(error)
     }
+
+    res.sendStatus(error.cause as number || 400)
 }
 
 export { errorHandler }
