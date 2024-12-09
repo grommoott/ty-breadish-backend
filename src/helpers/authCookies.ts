7b4e24499350c125606a9c172552830b63976110
grommoott <grommoott@gmail.com>
2024-12-09 22:28:45 +0700

    fix

--- src/helpers/authCookies.ts
+++ src/helpers/authCookies.ts
@@ -3,9 +3,9 @@ import config from "../config";
 import { minute, week, year } from "./timeConstants";
 
 function setAuthCookies(res: Response, accessToken: string, refreshToken: string, deviceId: string) {
-    res.cookie("RefreshToken", refreshToken, { secure: true, httpOnly: true, sameSite: "none", domain: config.backendDomain, maxAge: 2 * week })
-    res.cookie("AccessToken", accessToken, { secure: true, httpOnly: true, sameSite: "none", domain: config.backendDomain, maxAge: 20 * minute })
-    res.cookie("DeviceId", deviceId, { secure: true, httpOnly: true, domain: config.backendDomain, sameSite: "none", maxAge: 1000 * year })
+    res.cookie("RefreshToken", refreshToken, { secure: true, httpOnly: true, sameSite: "none", domain: config.backendUrl, maxAge: 2 * week })
+    res.cookie("AccessToken", accessToken, { secure: true, httpOnly: true, sameSite: "none", domain: config.backendUrl, maxAge: 20 * minute })
+    res.cookie("DeviceId", deviceId, { secure: true, httpOnly: true, domain: config.backendUrl, sameSite: "none", maxAge: 1000 * year })
 }
 
 function clearAuthCookies(res: Response) {
