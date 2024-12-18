de4399d94d5a28d6fdea65c279aef637b8098509
grommoott <grommoott@gmail.com>
2024-12-18 23:00:08 +0700

    18dec 24

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default {
     backendUrl: "ty-breadish-backend.onrender.com",
     frontendUrl: "https://ty-breadish.onrender.com",
-    maptilerApiKey: process.env.MAPTILER_API_KEY,
-    yookassaPassword: process.env.YOOKASSA_PASSWORD,
-    yookassaUsername: process.env.YOOKASSA_USERNAME,
-    mailPassword: process.env.MAIL_PASSWORD
+    maptilerApiKey: process.env.MAPTILER_API_KEY || "",
+    yookassaPassword: process.env.YOOKASSA_PASSWORD || "",
+    yookassaUsername: process.env.YOOKASSA_USERNAME || "",
+    mailPassword: process.env.MAIL_PASSWORD || ""
 }
