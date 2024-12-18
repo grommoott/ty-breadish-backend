410c0efcd5ac1012c12f135f52a23d153bf24c2c
grommoott <grommoott@gmail.com>
2024-12-18 22:47:12 +0700

    privacy

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export default {
     maptilerApiKey: process.env.MAPTILER_API_KEY,
     yookassaPassword: process.env.YOOKASSA_PASSWORD,
     yookassaUsername: process.env.YOOKASSA_USERNAME,
-    mailPassword: "mV2z5Tb11DzpSBik4AXd"
+    mailPassword: process.env.MAIL_PASSWORD
 }
